3607f92f2f3325949ad2c36fcc695f9a30acf0b6
Artur <arczigrodel2@wp.pl>
2024-06-03 20:03:08 +0200

    Initial commit

---
+++ scrap/src/main/java/webscrap/scrap/Book.java
@@ -0,0 +1,29 @@
+package webscrap.scrap;
+
+import lombok.Data;
+
+public class Book {
+    private String title;
+    private String price;
+
+    public Book(String title, String price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ scrap/src/main/java/webscrap/scrap/Scraper.java
@@ -0,0 +1,32 @@
+package webscrap.scrap;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scraper {
+        public List<Book> scrapeBooks() {
+            String url = "https://books.toscrape.com/";
+            List<Book> bookList = new ArrayList<>();
+            try {
+                Document document = Jsoup.connect(url).get();
+                Elements books = document.select(".product_pod");
+
+                for (Element bk : books) {
+                    String title = bk.select("h3 > a").text();
+                    String price = bk.select(".price_color").text();
+                    bookList.add(new Book(title, price));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bookList;
+        }
+    }
+
